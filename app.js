ce33fbd4f27022321f5bc9a100fc8c846d9a2144
Jeric <marianovjeric@gmail.com>
2023-01-13 00:48:25 -0800

    cleaner code of then example

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 
 const emailRef = document.querySelector(".email")
-console.log(emailRef)
-
 
 // 1. Then
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-    response.json().then(data => {
-        console.log(data)
-        emailRef.innerHTML = data.email
-    })
+    return response.json()
+}).then(data => {
+    console.log(data)
+    emailRef.innerHTML = data.email
 })
 
 // 2. Async/Await
