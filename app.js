45b4f6083d68f3a0d049b918d6e81779ef5f2416
Jeric <marianovjeric@gmail.com>
2023-01-13 00:01:13 -0800

    using the promise's data and adding it to the HTML

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 
+const emailRef = document.querySelector(".email")
+console.log(emailRef)
+
+
 // 1. Then
 
 fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
     response.json().then(data => {
         console.log(data)
+        emailRef.innerHTML = data.email
     })
 })
 
