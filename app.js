fdecbddb01d169bdb9a9660c8202fb4e05b988fa
Jeric <marianovjeric@gmail.com>
2023-01-12 23:49:16 -0800

    unlocking promise with then example

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+// 1. Then
+
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+    response.json().then(data => {
+        console.log(data)
+    })
+})
+
+// 2. Async/Await
